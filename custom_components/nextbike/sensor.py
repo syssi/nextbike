ac1d3b48081c3324216c3c1d9957f957edef19b4
Sebastian Muszynski <basti@linkt.de>
2026-06-25 12:02:15 +0200

    Modernize project setup (#15)

--- custom_components/nextbike/sensor.py
+++ custom_components/nextbike/sensor.py
@@ -5,13 +5,11 @@ https://github.com/syssi/nextbike
 """
 
 import asyncio
-import logging
 from datetime import timedelta
+import logging
 
 import aiohttp
 import async_timeout
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
     ATTR_LATITUDE,
@@ -24,11 +22,13 @@ from homeassistant.const import (
 )
 from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import async_track_time_interval
 from homeassistant.util import location
 from homeassistant.util.unit_conversion import DistanceConverter
 from homeassistant.util.unit_system import METRIC_SYSTEM
+import voluptuous as vol
 
 _LOGGER = logging.getLogger(__name__)
 
